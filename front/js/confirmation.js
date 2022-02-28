70c1ce8a510d87428f56d275e0dba7e93ea83c84
Hanane Mjaouel <hananemjaouel@MacBook-Air-de-Hanane.local>
2022-02-28 16:40:26 +0100

    final

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,6 @@
 //Je récupère les ID des produits
 const idOrder = new URL(window.location.href).searchParams.get("orderId");
-// fonction pour afficher le numéro de commande
+//fonction pour afficher le numéro de commande
 function orderIdDisplay (){
 const orderId = document.getElementById("orderId");
 orderId.innerText = idOrder;
